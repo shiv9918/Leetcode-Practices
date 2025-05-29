15eb49ee0468290d9892fd5b7caa8f94dbb1c511
Shivam Mishra <shivammishra01329@gmail.com>
2025-05-29 21:10:59 +0530

    Create ReverseLL.cpp

---
+++ ReverseLL.cpp
@@ -0,0 +1,12 @@
+ListNode* reverseList(ListNode* head) {
+        if(head == nullptr || head->next == nullptr)
+        {
+            return head;
+        }
+
+          ListNode*newHead = reverseList(head->next);
+
+          head->next->next = head;
+          head->next = nullptr;
+          return newHead;
+    }
